17531914292ab9b4e2e3b1eb901a8bda85c594a1
Daricus_Duncan <drdunkan15@gmail.com>
2017-02-22 23:33:33 -0600

    My food app

---
+++ Pods/Target Support Files/Pods-TasteBud/Pods-TasteBud-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TasteBud : NSObject
+@end
+@implementation PodsDummy_Pods_TasteBud
+@end
---
+++ Pods/Target Support Files/Pods-TasteBudUITests/Pods-TasteBudUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TasteBudUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TasteBudUITestsVersionString[];
+
